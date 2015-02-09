96fd01efffc53bd9fe5ba04a238110e77b9cd3dc
lution <lution@bydell.com>
2015-02-09 11:36:58 +0800

    add debugbar

--- app/config/app.php
+++ app/config/app.php
@@ -121,6 +121,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+                                    // Laravel-Debugbar see https://github.com/barryvdh/laravel-debugbar/tree/1.8
+                                    'Barryvdh\Debugbar\ServiceProvider',
 
 	),
 

0bc6a74b317b66705e050fa842afbe0a568476d0
Lution <lution@bydell.com>
2015-04-19 01:04:24 +0800

    swtich to local mysql server

--- app/config/database.php
+++ app/config/database.php
@@ -65,7 +65,7 @@ return array(
             
         'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '106.185.40.8',
+			'host'      => '127.0.0.1',
 			'database'  => 'platform_dev',
 			'username'  => 'platform_dev',
 			'password'  => 'DEV123',

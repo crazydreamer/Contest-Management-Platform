fcdb2403647299194550cd916721ed4611750b11
Lution <lution@bydell.com>
2015-05-11 22:57:21 +0800

    add getIp function

--- app/controllers/UtilsController.php
+++ app/controllers/UtilsController.php
@@ -32,4 +32,17 @@ class UtilsController extends BaseController {
         ));
     }
 
+    public static function getIp() {
+            global $ip;
+            if (getenv("HTTP_CLIENT_IP"))
+                $ip = getenv("HTTP_CLIENT_IP");
+            else if(getenv("HTTP_X_FORWARDED_FOR"))
+                $ip = getenv("HTTP_X_FORWARDED_FOR");
+            else if(getenv("REMOTE_ADDR"))
+                $ip = getenv("REMOTE_ADDR");
+            else $ip = "0.0.0.0";
+
+            return $ip;
+    }
+
 }
